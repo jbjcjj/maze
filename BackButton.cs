a88b462a229bb979c34a7f8a1208d3b035c3009f
jbjcjj <blablabla14042007@gmail.com>
2025-11-29 13:46:44 +0700

    BackButton.cs

--- BackButton.cs
+++ BackButton.cs
@@ -1,33 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // Dùng để chuyển scene
 
 [RequireComponent(typeof(Button))]
 [RequireComponent(typeof(Image))]
 public class BackToMenuButton : MonoBehaviour
 {
     [Header("Ảnh của nút")]
-    public Sprite buttonSprite; 
+    public Sprite buttonSprite; // Ảnh dùng làm hình của nút
 
-    private Button button;
-    private Image image;
+    private Button button;// Biến lưu component Button
+    private Image image;// Biến lưu component Image
 
     private void Awake()
     {
-        button = GetComponent<Button>();
-        image = GetComponent<Image>();
+        button = GetComponent<Button>();// Lấy component Button gắn trên GameObject
+        image = GetComponent<Image>();// Lấy component Image gắn trên GameObject
 
-        if (buttonSprite != null)
+        if (buttonSprite != null)// Nếu người dùng đã gắn sprite trong Inspector
         {
-            image.sprite = buttonSprite;
+            image.sprite = buttonSprite;// Đổi hình của nút sang sprite đã chọn
             image.type = Image.Type.Sliced; 
         }
 
-        button.onClick.AddListener(GoBackToMainMenu);
+        button.onClick.AddListener(GoBackToMainMenu);// Khi nút được nhấn → gọi hàm GoBackToMainMenu
     }
 
     public void GoBackToMainMenu()
     {
-        SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene("Main Menu");// Chuyển sang scene có tên "Main Menu"
     }
 }
+
