c89af5853c3d1a07a0c86cdf4cd2edfb78be0f4d
jbjcjj <blablabla14042007@gmail.com>
2025-11-29 13:50:34 +0700

    Victory.cs

--- Victory.cs
+++ Victory.cs
@@ -1,10 +1,10 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine;                
+using UnityEngine.SceneManagement; // Dùng để chuyển scene
 
 public class VictoryUI : MonoBehaviour
 {
     public void BackToMainMenu()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);  // Chuyển sang scene có index = 0 trong Build Settings
     }
 }
