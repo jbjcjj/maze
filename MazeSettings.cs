14edc4f8f2a67b3bb4f49df19e9115b511eda356
jbjcjj <blablabla14042007@gmail.com>
2025-11-29 13:52:27 +0700

    MazeSettings.cs

--- MazeSettings.cs
+++ MazeSettings.cs
@@ -1,5 +1,5 @@
-﻿public static class MazeSettings
+public static class MazeSettings
 {
-    public static int numX = 10;  
-    public static int numY = 10; 
+    public static int numX = 10;   // Số lượng phòng theo trục X (chiều ngang), mặc định = 10
+    public static int numY = 10;   // Số lượng phòng theo trục Y (chiều dọc), mặc định = 10
 }
