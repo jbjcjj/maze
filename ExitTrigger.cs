029d91b8978d82abe8f43c9e97f7a5b578b81ae2
jbjcjj <blablabla14042007@gmail.com>
2025-11-29 13:51:52 +0700

    ExitTrigger.cs

--- ExitTrigger.cs
+++ ExitTrigger.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine;                 
+using UnityEngine.SceneManagement; // Dùng để chuyển scene
 
 public class ExitTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Log ra tên object vừa chạm vào trigger (để debug)
         Debug.Log("Trigger collided with: " + collision.name);
+
+        // Kiểm tra xem object chạm vào có Tag là "Player" hay không
         if (collision.CompareTag("Player"))
         {
+            // Log để biết người chơi đã đến điểm thoát
             Debug.Log("Player reached exit!");
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(2);// Chuyển sang scene có index = 2 trong Build Settings
         }
     }
 }
